9b1191b0652f776721c5abb2a19219d964b0aa3d
Martin148 <martinwehner148@googlemail.com>
2023-03-18 21:30:08 +0100

    chore - add button tests (#31)
    Co-authored-by: Martin Wehner <martin.wehner@dev-creations.de>

--- packages/marica-react/src/components/Button/index.tsx
+++ packages/marica-react/src/components/Button/index.tsx
@@ -1,7 +1,14 @@
 import { PropsWithChildren } from "react";
 import "./Button.css";
 
-export type MaiButtonProps = { } & React.ButtonHTMLAttributes<HTMLButtonElement>;
+export type MaiButtonProps = { 
+  /** 
+   * Set disabled to true to make the button inactive. 
+   * 
+   * @default false
+  */
+  disabled?: boolean
+ } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const MaiButton = (props: PropsWithChildren<MaiButtonProps>) => {
   const {children, type = 'button'} = props;
