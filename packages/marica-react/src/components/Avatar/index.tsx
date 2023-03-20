5936723602c248f620d87e1caebb90facbb05980
Enrico Gruner <enrico.gruner@dev-creations.de>
2023-03-20 17:56:25 +0100

    fix - allow custom title for mai-avatar (#35)

--- packages/marica-react/src/components/Avatar/index.tsx
+++ packages/marica-react/src/components/Avatar/index.tsx
@@ -5,13 +5,14 @@ type AvatarProps = {
   image?: string;
   notifications?: number;
   onClick?: () => void;
+  title?: string;
 }
 
-export const MaiAvatar = ({ username, image, onClick, notifications }: AvatarProps) => {
+export const MaiAvatar = ({ username, image, onClick, notifications, title }: AvatarProps) => {
   const initials = username.split(" ").map((s: string) => s.charAt(0)).join("").slice(0,2).trim();
 
   return (
-    <button type="button" className="mai-avatar" onClick={onClick} title={username + " avatar"} data-notifications={notifications}>
+    <button type="button" className="mai-avatar" onClick={onClick} title={ title || username + " avatar"} data-notifications={notifications}>
       {!image ? <span>
         {initials}
       </span> : <img src={image} alt="" role="presentation" width="32" height="32" />}
