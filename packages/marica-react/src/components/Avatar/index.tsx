e68b169ea556d0e4e9115bd40f9ebccda245fcea
Enrico Gruner <enrico.gruner@dev-creations.de>
2023-05-24 14:06:54 +0200

    fix - server avatar props on button

--- packages/marica-react/src/components/Avatar/index.tsx
+++ packages/marica-react/src/components/Avatar/index.tsx
@@ -8,11 +8,12 @@ type AvatarProps = {
   title?: string;
 }
 
-export const MaiAvatar = ({ username, image, onClick, notifications, title }: AvatarProps) => {
+export const MaiAvatar = (props: AvatarProps) => {
+  const { username, image, onClick, notifications, title } = props;
   const initials = username.split(" ").map((s: string) => s.charAt(0)).join("").slice(0,2).trim();
 
   return (
-    <button type="button" className="mai-avatar" onClick={onClick} title={ title || username + " avatar"} data-notifications={notifications}>
+    <button {...props} type="button" className="mai-avatar" onClick={onClick} title={ title || username + " avatar"} data-notifications={notifications}>
       {!image ? <span>
         {initials}
       </span> : <img src={image} alt="" role="presentation" width="32" height="32" />}
